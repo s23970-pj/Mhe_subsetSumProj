6047bb4f9b43bc78f17a9cd240fb955ebc06caf1
Adrian <adekgoik58@gmail.com>
2024-06-24 18:43:11 +0200

    -tabu algorithm with tabu_history list enabling the algorithm to go back when reaching local optimum -also added desrciption of current iteration for future better understanding of the algorithm

--- src/tabu_algorithm.py
+++ src/tabu_algorithm.py
@@ -13,13 +13,24 @@ def tabu_search(elements, target, r, tabu_size):
     best_solution = current_solution
     best_residue = residue(current_solution, elements, target)
     tabu_list = []
+    tabu_history = []
 
     for iteration in range(r):
+        print(f"iteracja {iteration+1}")
+        print(f"current solution: {[elements[i] for i in range(len(current_solution)) if current_solution[i] == 1]}")
+        print(f"Current Residue: {residue(current_solution, elements, target)}")
+        print(f"Best Solution so far: {[elements[i] for i in range(len(best_solution)) if best_solution[i] == 1]}")
+        print(f"Best Residue so far: {best_residue}")
+        print("=" * 40)
         neighbors = generate_neighbors(current_solution)
         feasible_neighbors = [n for n in neighbors if n not in tabu_list]
 
         if not feasible_neighbors:
-            break
+            if tabu_history: #sprawdzenie czy lista jest pusta
+                current_solution=tabu_history.pop() #wróć nazad
+                continue
+            else:
+                break
 
         best_neighbor = min(feasible_neighbors, key=lambda s: residue(s, elements, target))
         best_neighbor_residue = residue(best_neighbor, elements, target)
@@ -31,7 +42,8 @@ def tabu_search(elements, target, r, tabu_size):
         tabu_list.append(current_solution)
         if len(tabu_list) > tabu_size:
             tabu_list.pop(0)
-
+        tabu_history.append(current_solution)
+        #print(tabu_history)
         current_solution = best_neighbor
 
     return best_solution, best_residue
